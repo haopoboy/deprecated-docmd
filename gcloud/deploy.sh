d4f144302d998f3ca4564f5cdd17f565bf15dcb0
haopo <haopo@192.168.1.106>
2017-10-21 16:03:43 +0800

    Try to push to google cloud.

--- gcloud/deploy.sh
+++ gcloud/deploy.sh
@@ -2,8 +2,13 @@
 
 set -e
 
+docker build -t "$DOCKER_USERNAME/$PROJECT_NAME" -t "gcr.io/$GCLOUD_PROJECT_ID/$PROJECT_NAME" .;
+
 if [ "$TRAVIS_BRANCH" == "master" ]; then
 	docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
-	docker build -t "$DOCKER_USERNAME/$PROJECT_NAME:latest" .;
-	docker push "$DOCKER_USERNAME/$PROJECT_NAME:latest";
+	docker push "$DOCKER_USERNAME/$PROJECT_NAME";
 fi
+
+echo $GCLOUD_SERVICE_KEY | base64 --decode -i > $GOOGLE_APPLICATION_CREDENTIALS
+gcloud auth activate-service-account --key-file $GOOGLE_APPLICATION_CREDENTIALS
+gcloud docker -- push "gcr.io/$GCLOUD_PROJECT_ID/$PROJECT_NAME"
