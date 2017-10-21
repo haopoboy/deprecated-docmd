61c502fcef156d09494fdf6176a10abb28eec924
haopo <haopo@192.168.1.106>
2017-10-21 14:51:59 +0800

    Deploy to docker hub.

--- gcloud/deploy.sh
+++ gcloud/deploy.sh
@@ -2,4 +2,8 @@
 
 set -e
 
-docker build -t docmd:latest .
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+	docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
+	docker build -t "$PROJECT_NAME:latest" .;
+	docker push "$DOCKER_USERNAME/$PROJECT_NAME";
+fi
