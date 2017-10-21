3bbfcec151dba035a35a8432b665d2b390154f7c
haopo <haopo@192.168.1.106>
2017-10-21 15:01:45 +0800

    Add tag.

--- gcloud/deploy.sh
+++ gcloud/deploy.sh
@@ -5,5 +5,5 @@ set -e
 if [ "$TRAVIS_BRANCH" == "master" ]; then
 	docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
 	docker build -t "$PROJECT_NAME:latest" .;
-	docker push "$DOCKER_USERNAME/$PROJECT_NAME";
+	docker push "$DOCKER_USERNAME/$PROJECT_NAME:latest";
 fi
