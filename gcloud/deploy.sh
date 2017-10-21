b581c14e0fb19cac7332e76bda07801abd020dfd
haopo <haopo@192.168.1.106>
2017-10-21 15:10:18 +0800

    Try to fix push.

--- gcloud/deploy.sh
+++ gcloud/deploy.sh
@@ -4,6 +4,6 @@ set -e
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
 	docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
-	docker build -t "$PROJECT_NAME:latest" .;
+	docker build -t "$DOCKER_USERNAME/$PROJECT_NAME:latest" .;
 	docker push "$DOCKER_USERNAME/$PROJECT_NAME:latest";
 fi
